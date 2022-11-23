e12e4b250c13683e5dc8c2f2179832108b087b69
Daylan Merritt <daylan.merritt@gmail.com>
2022-11-22 20:39:13 -0700

    validations and notes

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :contact
+
+  validates :street, :zip, :state, :city, presence: true
+  validates :zip, numericality: { only_integer: true}
+  
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :note
+
+  validates :body, presence: true
+
 end
